6fe4a7eb5fd085b219964e933a1a729e8f5b2096
Richard Pisarski <rspisarski@gmail.com>
2025-02-10 19:00:51 -0500

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import alpine from '@astrojs/alpinejs';
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  integrations: [
+    alpine({
+      // Initialize Alpine.js on page load
+      entrypoint: '/src/alpine.js'
+    })
+  ]
 });
--- src/data/menu.ts
+++ src/data/menu.ts
@@ -1,10 +1,27 @@
 // src/data/menu.ts
 
 export const headerMenu = [
-    { name: 'Home', link: '/' },
-    { name: 'Blog', link: '/blog' },
+    { name: 'About', link: '/' },
+    { name: 'Style-Guide', link: '/style-guide', showArrow: false,
+        children: [
+            { name: 'Typography', link: '/style-guide/typography' },
+            { name: 'Colors', link: '/style-guide/colors' },
+            { name: 'Buttons', link: '/style-guide/buttons' },
+            { name: 'Forms', link: '/style-guide/forms' },
+        ]
+    },
+    { name: 'Contact', link: '/contact', showArrow: false,
+        children: [
+            { name: 'Contact Us', link: '/contact/contact-us' },
+            { name: 'Support', link: '/contact/support' },
+        ] 
+    },
+
   ];
   
+
+
+
   export const footerMenu = [
     { name: 'Style Guide', link: '/style-guide' },
   ];
