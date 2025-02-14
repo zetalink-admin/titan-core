2ac11fcff4efa4600791f743e3e3e64739c08ed0
Richard Pisarski <rspisarski@gmail.com>
2025-02-13 22:44:53 -0500

    updates

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,18 +1,21 @@
 export const siteConfig = {
-    companyName: 'Titan Core',
-    Socials: {
-        Twitter: 'https://twitter.com/astrodotbuild',
-        Github: 'https://github.com/withastro',
-        Instagram: 'https://www.instagram.com/astrodotbuild',
-    }
-  };
+  companyName: 'Titan Core',
+  Socials: {
+      Twitter: 'https://twitter.com/astrodotbuild',
+      Github: 'https://github.com/withastro',
+      Instagram: 'https://www.instagram.com/astrodotbuild',
+  }
+};
 
-  export const SEO = {
-    Separator: '|',
-    SiteName: 'Astro Basics',
-  };
-  
+export const SEO = {
+  Separator: '|',
+  SiteName: 'Astro Basics',
+};
   
 export const blogSetting = {
-    postsPerPage: 6   
+  postsPerPage: 6   
+}
+
+export const themeSetting = {
+  theme: 'rainforest'
 }
