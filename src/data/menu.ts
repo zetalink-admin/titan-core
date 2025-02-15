d84f88b0c87ab21e674d9b7a39865a699588097f
Richard Pisarski <rspisarski@gmail.com>
2025-02-15 12:31:41 -0500

    updates

--- src/data/menu.ts
+++ src/data/menu.ts
@@ -5,19 +5,14 @@ export const headerMenu = [
     { name: 'Blog', link: '/blog' },
     { name: 'Style-Guide', link: '/style-guide', showArrow: false,
         children: [
-            { name: 'Typography', link: '/style-guide/typography' },
-            { name: 'Colors', link: '/style-guide/colors' },
-            { name: 'Buttons', link: '/style-guide/buttons' },
-            { name: 'Forms', link: '/style-guide/forms' },
+            { name: 'Typography', link: '/style-guide#typography' },
+            { name: 'Colors', link: '/style-guide#colors' },
+            { name: 'Links', link: '/style-guide#links' },
+            { name: 'Buttons', link: '/style-guide#buttons' },
+            { name: 'Forms', link: '/style-guide#forms' },
+            { name: 'Lists', link: '/style-guide#lists' },
         ]
-    },
-    { name: 'Contact', link: '/contact', showArrow: false,
-        children: [
-            { name: 'Contact Us', link: '/contact/contact-us' },
-            { name: 'Support', link: '/contact/support' },
-        ] 
-    },
-
+    }
 ];
 
 export const footerMenu = [
