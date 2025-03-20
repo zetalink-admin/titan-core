29fe21ae3c2c573ae23569fe3ecb9eef88274702
Tamara Cook <info@infolektuell.de>
2025-03-20 14:18:05 +0100

    Add prettier and astro check for better code quality

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+/** @type {import('prettier').Options} */
+export default {
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+  singleQuote: true,
+  astroAllowShorthand: true,
+}
